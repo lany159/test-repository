38457a264e641e1a596c60a37883e1678b583245
lany159 <tnfl2644@gmail.com>
2024-03-20 17:14:16 +0900

    introduction_lv2

--- app/src/main/java/com/example/introduction/SignUpActivity.kt
+++ app/src/main/java/com/example/introduction/SignUpActivity.kt
@@ -1,11 +1,39 @@
 package com.example.introduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btn = findViewById<Button>(R.id.btn_login)
+        val et_name = findViewById<EditText>(R.id.et_name)
+        val et_id = findViewById<EditText>(R.id.et_id)
+        val et_pw = findViewById<EditText>(R.id.et_pw)
+
+        btn.setOnClickListener {
+            if (et_name.text.isEmpty()){
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (et_id.text.isEmpty()){
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (et_pw.text.isEmpty()){
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
