c296dd6f93b0055b0c185f00cd94449cff4862a6
lany159 <tnfl2644@gmail.com>
2024-03-20 17:51:53 +0900

    introduction_lv3

--- app/src/main/java/com/example/introduction/HomrActivity.kt
+++ app/src/main/java/com/example/introduction/HomrActivity.kt
@@ -1,11 +1,24 @@
 package com.example.introduction
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomrActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homr)
+
+        val btn = findViewById<Button>(R.id.btn_end)
+        val tv_id = findViewById<TextView>(R.id.tv_id)
+        val strData = intent.getStringExtra("login")
+        tv_id.setText(strData)
+
+        btn.setOnClickListener {
+            finish()
+        }
     }
 }
