61c09dba6693edcae61c4d2d73efd4759843082a
lany159 <tnfl2644@gmail.com>
2024-03-21 21:13:30 +0900

    introduction_lv4

--- app/src/main/java/com/example/introduction/SignInActivity.kt
+++ app/src/main/java/com/example/introduction/SignInActivity.kt
@@ -6,40 +6,59 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    //ActivityResultLauncher라는 타입 resultLauncher 선언
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.btn_login)
         val btn_signUp = findViewById<Button>(R.id.btn_signup)
-        val ed_id = findViewById<EditText>(R.id.ev_id)
-        val ed_pw = findViewById<EditText>(R.id.ev_pw)
+        val et_id = findViewById<EditText>(R.id.et_id)
+        val et_pw = findViewById<EditText>(R.id.et_pw)
 
         btn.setOnClickListener {
 
-            if (ed_id.text.isEmpty()){
-                Toast.makeText(this, "아이디를 입력해주세요",Toast.LENGTH_SHORT).show()
+            if (et_id.text.isEmpty()) {
+                Toast.makeText(this, "아이디를 입력해주세요", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if (ed_pw.text.isEmpty()){
+            if (et_pw.text.isEmpty()) {
                 Toast.makeText(this, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
 
-            val strData = ed_id.text.toString()
+            val strData = et_id.text.toString()
             val intent = Intent(this, HomrActivity::class.java)
-            intent.putExtra("login",strData)
+            intent.putExtra("login", strData)
             startActivity(intent)
         }
 
         btn_signUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            resultLauncher.launch(intent)
+            //StartActivityForResult() 동일 기능
         }
+
+        resultLauncher =
+            //resultLauncher 변수에 registerForActivityResult() 활용
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                //회원가입 엑티비티에서 결과 값을 받아올 수 있는 구문
+                if (result.resultCode == RESULT_OK) {
+                    //resultCode 의 역할? 기본값: RESULT_OK , 여러 화면 이동 가정, 데이터도 함께 이동, 구분이 필요하기 때문에 사용
+                    val id = result.data?.getStringExtra("id") ?: ""
+                    val pw = result.data?.getStringExtra("pw") ?: ""
+
+                    et_id.setText(id)
+                    et_pw.setText(pw)
+                }
+            }
     }
 }
 
--- app/src/main/java/com/example/introduction/SignUpActivity.kt
+++ app/src/main/java/com/example/introduction/SignUpActivity.kt
@@ -6,34 +6,42 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         val btn = findViewById<Button>(R.id.btn_login)
-        val et_name = findViewById<EditText>(R.id.et_name)
-        val et_id = findViewById<EditText>(R.id.et_id)
-        val et_pw = findViewById<EditText>(R.id.et_pw)
 
         btn.setOnClickListener {
-            if (et_name.text.isEmpty()){
-                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            if (et_id.text.isEmpty()){
-                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            if (et_pw.text.isEmpty()){
-                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
 
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
+            val et_name = findViewById<EditText>(R.id.et_name)
+            val strName = et_name.text.toString()
+            val et_id = findViewById<EditText>(R.id.et_id)
+            val strId = et_id.text.toString()
+            val et_pw = findViewById<EditText>(R.id.et_pw)
+            val strPw = et_pw.text.toString()
+
+            val isName = strName.isEmpty()
+            val isId = strId.isEmpty()
+            val isPw = strPw.isEmpty()
+
+            if (isName || isId || isPw){
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+            }else{
+                intent.putExtra("id",strId)
+                intent.putExtra("pw",strPw)
+                setResult(RESULT_OK, intent)
+                //RESULT_OK는 데이터를 실어서 보내는 역할인가?
+                //-1을 RESULT_OK로 받는 것
+                //setResult가 데이터를 실어서 보내는 역할인가?
+                //intent 생략 가능
+                finish()
+            }
         }
     }
 }
